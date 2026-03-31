687610692c24c1dc4d8612898d30a5a30b8aede1
sadwiik06 <2300032291@kluniversity.in>
2026-03-31 12:14:20 +0530

    feat: initialize Spring Boot backend project with database, security, and environment configuration

--- TaskPlatformBackend/src/main/java/com/sadwiik/taskplatform/TaskplatformApplication.java
+++ TaskPlatformBackend/src/main/java/com/sadwiik/taskplatform/TaskplatformApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskplatformApplication {
 
 	public static void main(String[] args) {
+		io.github.cdimascio.dotenv.Dotenv dotenv = io.github.cdimascio.dotenv.Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(TaskplatformApplication.class, args);
 	}
 
